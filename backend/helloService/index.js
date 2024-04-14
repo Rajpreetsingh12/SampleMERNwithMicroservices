899df1601b0701bb73322c7f2f0ddbcf40b3a10f
unknown <rajpreetsinghjahagirdar59@gmail.com>
2024-04-15 00:39:32 +0530

    wip: Deploy changes

--- backend/helloService/index.js
+++ backend/helloService/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.get('/', (req,res)=>{
     res.send({msg: 'Hello World'})
 })
-app.get('/health', (req,res)=>{
+app.get('/hello/health', (req,res)=>{
     res.send({status: 'OK'})
 })
 
